13e5009cef0b80074c58cd2f8ef44293247e3833
Kyler <kylerlepp8@gmail.com>
2022-06-13 14:07:43 -0700

    refactored getAllReservations using sql query to get a users reservations from the database

--- LightBnB_WebApp-master/LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/LightBnB_WebApp-master/server/database.js
@@ -87,7 +87,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  let user;
+  return pool
+  .query(`SELECT properties.number_of_bedrooms, properties.parking_spaces, properties.thumbnail_photo_url, properties.number_of_bathrooms, reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    user = result.rows;
+    if (!user) {
+      return null;
+    }
+    return Promise.resolve(user);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
